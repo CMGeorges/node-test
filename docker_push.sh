bac4f9f26c57a2d2919dd6bbd8cf222a05f8d084
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-02-18 22:01:49 -0500

    test deploy docker 5

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$PWD" | docker login -u "$USERNAME" --password-stdin
+echo "$PWD" | docker login -u "$USERNAME" --password-stdin 61295187+CMGeorges@users.noreply.github.com
 docker push cmgeorges/node-test
